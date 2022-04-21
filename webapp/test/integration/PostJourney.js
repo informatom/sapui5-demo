f5b267e78f5144dbf4a660465d7cffb777738494
Stefan Haslinger <stefan.haslinger@mittenin.at>
2022-04-21 16:42:26 +0000

    testing: step 13: testing user interaction

--- webapp/test/integration/PostJourney.js
+++ webapp/test/integration/PostJourney.js
@@ -17,7 +17,7 @@ sap.ui.define([
 
 		// Assertions
 		Then.onThePostPage.theTitleShouldDisplayTheName("Jeans");
-	});
+  });
 
 	opaTest("Should go back to the TablePage", function (Given, When, Then) {
 		// Actions
@@ -33,8 +33,13 @@ sap.ui.define([
 
 		// Assertions
 		Then.onThePostPage.theTitleShouldDisplayTheName("Jeans");
-
-		// Cleanup
-		Then.iTeardownMyApp();
 	});
+
+  opaTest("Should select the statistics tab", function (Given, When, Then) {
+    // Actions
+    When.onThePostPage.iPressOnTheTabWithTheKey("statistics");
+    // Assertions
+    Then.onThePostPage.iShouldSeeTheViewCounter()
+      .and.iTeardownMyApp();
+  });
 });

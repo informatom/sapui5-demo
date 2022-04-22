0b1a99e4b995dfe19372788c4c7fe52b810ed8e7
Stefan Haslinger <mail@stefan-haslinger.at>
2022-04-22 13:10:39 +0200

    flexible column layout app: step 1: setting up the initial app

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,22 +1,12 @@
 sap.ui.define([
-	"sap/ui/core/UIComponent"
+	'sap/ui/core/UIComponent'
 ], function(UIComponent) {
-	"use strict";
+	'use strict';
 
-	return UIComponent.extend("sap.ui.demo.MockServer.Component", {
+	return UIComponent.extend('sap.ui.demo.fiori2.Component', {
 
 		metadata: {
-			manifest: "json"
-		},
-
-		/**
-		 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
-		 * @public
-		 * @override
-		 */
-		init: function() {
-			// call the base component's init function
-			UIComponent.prototype.init.apply(this, arguments);
+			manifest: 'json'
 		}
 	});
 });

15c19a94b53d8c6ebc016a87dfd6172bf2277a41
Stefan Haslinger <stefan.haslinger@mittenin.at>
2022-04-25 12:31:46 +0000

    smart controls: step 7d view manangement

--- webapp/VariantManagement.controller.js
+++ webapp/VariantManagement.controller.js
@@ -2,5 +2,7 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller"
 ], function(Controller) {
 	"use strict";
-	return Controller.extend("sap.ui.demo.smartControls.SmartTable");
+
+	return Controller.extend("sap.ui.demo.smartControls.VariantManagement");
+
 });

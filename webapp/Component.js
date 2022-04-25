fa5a6fe363b218a19f20730c2c546cb0fe679e83
Stefan Haslinger <stefan.haslinger@mittenin.at>
2022-04-25 13:39:07 +0000

    3d-viewer: step 1: 3d viewer with single file loading

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,16 +1,17 @@
 sap.ui.define([
   "sap/ui/core/UIComponent"
-], function(UIComponent) {
-"use strict";
-return UIComponent.extend("sap.ui.demo.smartControls.Component", {
-  metadata: {
-      manifest: "json"
-  },
-  init: function () {
-      UIComponent.prototype.init.apply(this, arguments);
-  },
-  destroy: function () {
-          UIComponent.prototype.destroy.apply(this, arguments);
-  }
-});
+], function (UIComponent) {
+  "use strict";
+
+  return UIComponent.extend("singleFile.Component", {
+
+      metadata: {
+          manifest: "json"
+      },
+
+      init: function () {
+          // call the init function of the parent
+          UIComponent.prototype.init.apply(this, arguments);
+      }
+  });
 });

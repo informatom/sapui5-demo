9a113fa9165ca72b4048bbdac734eabd8b9eb60a
Stefan Haslinger <mail@stefan-haslinger.at>
2022-04-25 08:37:30 +0200

    smart-controls: step 1: smart field

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,50 +1,20 @@
-sap.ui.define(['sap/ui/core/UIComponent'],
-	function(UIComponent) {
-		"use strict";
+sap.ui.define([
+	'sap/ui/core/UIComponent', 'sap/ui/fl/FakeLrepConnectorLocalStorage'
+], function(UIComponent, FakeLrepConnectorLocalStorage) {
+	"use strict";
 
-		return UIComponent.extend("sap.rules.ui.sample.TextRule.Component", {
-
-			metadata: {
-				"rootView": "sap.rules.ui.sample.TextRule.Page",
-				"dependencies": {
-					"libs": {
-						"sap.ui.core": {},
-						"sap.m": {},
-						"sap.ui.layout": {},
-						"sap.rules.ui": {}
-					}
-				},
-				config: {
-					sample: {
-						stretch: true,
-						files: [
-							"Page.view.xml",
-							"Page.controller.js",
-							"localService/vocabulary/mockdata/metadata.xml",
-							"localService/vocabulary/mockdata/Vocabularies.json",
-							"localService/vocabulary/mockdata/Associations.json",
-							"localService/vocabulary/mockdata/Attributes.json",
-							"localService/vocabulary/mockdata/DataObjects.json",
-							"localService/vocabulary/mockdata/Rules.json",
-							"localService/vocabulary/mockdata/ValueHelps.json",
-							"localService/rule/mockdata/metadata.xml",
-							"localService/rule/mockdata/Rules.json",
-							"localService/rule/mockdata/TextRuleResultExpressions.json",
-							"localService/rule/mockdata/TextRuleResults.json",
-							"localService/rule/mockdata/TextRuleConditions.json",
-							"localService/rule/mockdata/TextRules.json",
-							"localService/rule/mockdata/Projects.json",
-							"localService/rule/mockdata/RuleServices.json",
-							"localService/rule/mockdata/RulesetRules.json",
-							"localService/rule/mockdata/RulesetRuleServices.json",
-							"localService/rule/mockdata/Rulesets.json",
-							"localService/rule/mockdata/responses.json"
-						] 
-					}
-				}
-
-			}
-
-		});
-
+	return UIComponent.extend("sap.ui.demo.smartControls.Component", {
+		metadata: {
+			manifest: "json"
+		},
+		init: function() {
+			FakeLrepConnectorLocalStorage.enableFakeConnector(sap.ui.require.toUrl("sap/ui/demo/smartControls/lrep/component-test-changes.json"));
+			UIComponent.prototype.init.apply(this, arguments);
+		},
+		destroy: function() {
+			FakeLrepConnectorLocalStorage.disableFakeConnector();
+			UIComponent.prototype.destroy.apply(this, arguments);
+		}
 	});
+
+});

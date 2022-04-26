bc841d333b0e7b4a407fff846a91105e475df239
Stefan Haslinger <stefan.haslinger@mittenin.at>
2022-04-26 09:23:56 +0000

    ice-cream-machine: step 11: process flow

--- webapp/controller/ProcessFlow.controller.js
+++ webapp/controller/ProcessFlow.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
+  "sap/m/MessageToast",
 	"sap/base/strings/formatMessage"
-], function (Controller, formatMessage) {
+], function (Controller, MessageToast, formatMessage) {
 	"use strict";
 
 	return Controller.extend("sap.ui.vk.tutorial.icecream.controller.ProcessFlow", {
@@ -13,6 +14,15 @@ sap.ui.define([
 
     getValuesDelta: function(fFirstValue, fSecondValue) {
       return fSecondValue - fFirstValue;
+    },
+
+    onNodePressed: function(oEvent) {
+      var sItemTitle = oEvent.getParameters().getTitle();
+      MessageToast.show(this.getResourceBundle().getText("processFlowNodeClickedMessage", [sItemTitle]));
+    },
+
+    getResourceBundle: function() {
+        return this.getOwnerComponent().getModel("i18n").getResourceBundle();
     }
 	});
 });

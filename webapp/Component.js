7e1b2d3dc67ebc4fed1dd9a289a4a781d4bb6f55
Stefan Haslinger <mail@stefan-haslinger.at>
2022-04-26 09:07:23 +0200

    ice-cream-machine: step 1: initial application

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,17 +1,36 @@
 sap.ui.define([
-	"sap/ui/core/UIComponent"
-], function(UIComponent) {
+	"sap/ui/core/UIComponent",
+	"./model/models"
+], function (UIComponent, models) {
 	"use strict";
 
-	return UIComponent.extend("viewportScenetreeStepnav.Component", {
+	return UIComponent.extend("sap.suite.ui.commons.demokit.tutorial.icecream.01.Component", {
 
 		metadata: {
 			manifest: "json"
 		},
 
-		init: function() {
-			// call the init function of the parent
+		/**
+		 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+		 * @public
+		 * @override
+		 */
+		init: function () {
+			// call the base component's init function
 			UIComponent.prototype.init.apply(this, arguments);
+
+			// set the device model
+			this.setModel(models.createDeviceModel(), "device");
+
+			// create the views based on the url/hash
+			this.getRouter().initialize();
+		},
+		createContent: function () {
+			// create root view
+			return sap.ui.view("AppView", {
+				viewName: "sap.suite.ui.commons.demokit.tutorial.icecream.01.view.App",
+				type: "XML"
+			});
 		}
 	});
 });

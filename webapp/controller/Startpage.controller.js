988a3595c0a2487d68bcbd1502ef9f92d64da26d
Stefan Haslinger <stefan.haslinger@mittenin.at>
2022-04-26 07:34:28 +0000

    ice-cream-machine: step 3: launch tile and slide tile

--- webapp/controller/Startpage.controller.js
+++ webapp/controller/Startpage.controller.js
@@ -1,12 +1,20 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
+  "sap/ui/model/json/JSONModel",
   "sap/ui/core/format/NumberFormat",
   "sap/base/strings/formatMessage"
-], function (Controller, NumberFormat) {
+], function (Controller, JSONModel, NumberFormat, formatMessage) {
 	"use strict";
 
 	return Controller.extend("sap.suite.ui.commons.demokit.tutorial.icecream.controller.Startpage", {
-		onInit: function () {},
+    onInit: function() {
+      var sDataPath = sap.ui.require.toUrl("sap/suite/ui/commons/demokit/tutorial/icecream/model/data") + "/News.json";
+      var oModel = new JSONModel(sDataPath);
+      this.getView().setModel(oModel, "news");
+    },
+
+    formatMessage: formatMessage,
+
     getProgress: function(aNodes) {
       if (!aNodes || aNodes.length === 0) {
          return 0;
@@ -17,16 +25,23 @@ sap.ui.define([
       }
       var fPercent = (iSum / aNodes.length) * 100;
       return fPercent.toFixed(0);
-   },
-   getEntityCount: function(entities) {
+    },
+
+    getEntityCount: function(entities) {
       return entities && entities.length || 0;
-   },
-   formatNumber: function(value) {
+    },
+
+    formatNumber: function(value) {
       var oFloatFormatter = NumberFormat.getFloatInstance({
           style: "short",
           decimals: 1
       });
       return oFloatFormatter.format(value);
+    },
+
+    formatJSONDate: function(date) {
+      var oDate = new Date(Date.parse(date));
+      return oDate.toLocaleDateString();
     }
 	});
 });
